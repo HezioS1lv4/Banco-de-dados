a371873813e3b91fedb230a7561f568399cf704a
Hézio Silva <103318917+HezioS1lv4@users.noreply.github.com>
2023-04-20 10:45:53 -0300

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -78,40 +78,23 @@ values
 (6, 'Livro - Redes de Computadores', 3, 5, 200.00, '2014-03-10'),
 (7, 'Livro - Guyton & Hall - Tratado de Fisiologia', 2, 2, 300.00, '2014-04-10');
 
+-- ###############################################
 
-select * from tb_livro;
+select * from infosBiblioteca;
 
+use infosBiblioteca;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+select * from tb_livro where preco > 200;
+select * from tb_livro where preco = 100;
+select * from tb_livro where titulo = 'redes';
+select * from tb_livro where titulo like '%redes%';
+select * from tb_livro where titulo like '%banco de dado%';
+select * from tb_livro where preco between 150 and 250;
+select * from tb_livro limit 3;
+select * from tb_livro order by preco desc;
+select * from tb_livro order by preco desc limit 3;
+select * from tb_livro order by dt_livro desc limit 3;
+select * from tb_livro order by dt_livro limit 3;
+select * from tb_livro where cd_editora in (2,3);
+select * from tb_livro where cd_editora = 3 and preco = 200;
+select preco, titulo, dt_livro from tb_livro;
